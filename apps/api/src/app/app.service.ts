3283146f6381abebd6243eb05f10d429bf4c591d
František Žiačik <ziacik@gmail.com>
2020-06-23 22:42:41 +0200

    Switched to tabs

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -3,7 +3,7 @@ import { Message } from '@visual-ts/api-interfaces';
 
 @Injectable()
 export class AppService {
-  getData(): Message {
-    return { message: 'Welcome to api!' };
-  }
+	getData(): Message {
+		return { message: 'Welcome to api!' };
+	}
 }
--- apps/visual-ts/jest.config.js
+++ apps/visual-ts/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'visual-ts',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/visual-ts',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
+	name: 'visual-ts',
+	preset: '../../jest.config.js',
+	coverageDirectory: '../../coverage/apps/visual-ts',
+	snapshotSerializers: [
+		'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+		'jest-preset-angular/build/AngularSnapshotSerializer.js',
+		'jest-preset-angular/build/HTMLCommentSerializer.js',
+	],
 };
