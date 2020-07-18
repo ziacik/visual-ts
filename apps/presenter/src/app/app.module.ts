ae4a369fc6fd766fd071e97e0a62e3c00899c4f5
František Žiačik <ziacik@gmail.com>
2020-07-18 17:01:02 +0200

    NgxGraph added

--- apps/presenter/src/app/app.module.ts
+++ apps/presenter/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabled' })],
+	imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabled' }), NgxGraphModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
